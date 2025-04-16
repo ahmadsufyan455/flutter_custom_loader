e3722447d43c6fd2d3a1c06dcafd86e41e49bb92
ahmadsufyan455 <ahmadsufyan455@gmail.com>
2025-04-16 21:17:20 +0700

    refactor code to make it more clean

--- lib/custom_loader.dart
+++ lib/custom_loader.dart
@@ -6,6 +6,7 @@ class CustomLoader extends StatelessWidget {
   final double size;
   final Color color;
   final Duration duration;
+
   const CustomLoader({
     super.key,
     this.size = 50,
@@ -18,31 +19,41 @@ class CustomLoader extends StatelessWidget {
     return SizedBox(
       width: size,
       height: size,
-      child: Center(
-        child: TweenAnimationBuilder<double>(
-          tween: Tween(begin: 0.0, end: 1.0),
-          duration: duration,
-          builder: (context, value, child) {
-            return Transform.rotate(
-              angle: value * 6.28, // Full circle
-              child: Container(
-                width: size,
-                height: size,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(color: color, width: 4.0),
-                ),
-                child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: CircularProgressIndicator(
-                    strokeWidth: 3.0,
-                    valueColor: AlwaysStoppedAnimation<Color>(color),
-                  ),
-                ),
-              ),
-            );
-          },
-        ),
+      child: Center(child: _buildRotatingLoader()),
+    );
+  }
+
+  Widget _buildRotatingLoader() {
+    return TweenAnimationBuilder<double>(
+      tween: Tween(begin: 0.0, end: 1.0),
+      duration: duration,
+      builder: (context, value, child) {
+        return Transform.rotate(
+          angle: value * 6.28, // Full circle
+          child: _buildLoaderContainer(),
+        );
+      },
+    );
+  }
+
+  Widget _buildLoaderContainer() {
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(color: color, width: 4.0),
+      ),
+      child: _buildProgressIndicator(),
+    );
+  }
+
+  Widget _buildProgressIndicator() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: CircularProgressIndicator(
+        strokeWidth: 3.0,
+        valueColor: AlwaysStoppedAnimation<Color>(color),
       ),
     );
   }
